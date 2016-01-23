a3b201bf0e03f6ea08e299f193663bd8f11ed12a
Quinn Tran <ssquinntran@berkeley.edu>
2016-01-22 16:39:42 -0800

    pics

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+  get 'sponsors/index'
+
   get 'landing/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  resources :articles, :abouts, :recruitments, :sisters, :galleries, :contacts, :philanthropies, :events
+  resources :articles, :abouts, :recruitments, :sisters
+  resources :galleries, :contacts, :philanthropies
+  resources :events, :sponsors
 
   root 'landing#index'
 
